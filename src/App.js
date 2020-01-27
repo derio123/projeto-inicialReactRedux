7c000eb2b9fa273c97cab5c5ed9fd8b31d8cb2dd
Derio Matheus Ferreira Costa <deriomatheus6@gmail.com>
2020-01-27 10:14:30 -0300

    Usando library redux

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
 import React, { Component } from 'react';
 import './App.css';
-import Derio from './Component/user/derio';
+import { Link, Route } from 'react-router-dom';
+import routesConfig from './routes/RoutesConfig';
+/* import Derio from './Component/user/derio';
 import Footer from './Component/footer/footer';
 import Header from './Component/Header/header';
 import Forms from './Component/forms/forms';
-import List from './Component/list/list';
+import List from './Component/list/list'; */
 
 class App extends Component {
-  
+
   render() {
     return (
       <div className="App">
-        <Header></Header>
-          <Forms></Forms>
-          <Derio></Derio>
-          <List></List>
-        <Footer></Footer>
+        {/* <Header></Header>
+        <Forms></Forms>
+        <Derio></Derio>
+        <List></List>
+        <Footer></Footer> */}
+
+        <Link to="/">
+          Home
+        </Link>
+        <Link to="/user">
+          User
+        </Link>
+        {routesConfig.map((value, key) => {
+          return <Route key={key} path={value.path} component={value.component} exact={value.exact}>
+
+          </Route>
+        })}
       </div>
     );
   }
